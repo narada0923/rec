21459484f801b838dbbfdac8a50fe2b76670efa8
Narada Muni <naradamuni@Naradas-MacBook-Pro.local>
2023-06-08 23:05:09 +0800

    fc

---
+++ src/script.js
@@ -0,0 +1,96 @@
+const svg = d3.select("svg"),
+    width = +svg.attr("width"),
+    height = +svg.attr("height");
+
+const mark = [
+    {
+        long: 106.9057,
+        lat: 47.8864,
+    },
+];
+
+const marks = [
+    {
+        long: 102.779,
+        lat: 46.2729,
+    },
+    {
+        long: 100.1772,
+        lat: 49.6429,
+    },
+    {
+        long: 114.5356,
+        lat: 48.0951,
+    },
+    {
+        long: 95.539,
+        lat: 45.108,
+    },
+];
+
+var link = [];
+marks.forEach(function (row) {
+    source = [mark.long, mark.lat];
+    target = [row.long, row.lat];
+    topush = { type: "LineString", coordinates: [source, target] };
+    link.push(topush);
+});
+
+const projection = d3
+    .geoMercator()
+    .center([103.7, 47])
+    .scale(1200)
+    .translate([width / 2, height / 2]);
+
+var path = d3.geoPath().projection(projection);
+
+d3.json("../mn.geojson").then(function (data) {
+    svg.append("g")
+        .selectAll("path")
+        .data(data.features)
+        .join("path")
+        .attr("fill", "#2C8C7BA3")
+        .attr("d", d3.geoPath().projection(projection))
+        .attr("class", "province")
+        .style("stroke-width", 1)
+        .style("stroke", "none");
+
+    svg.selectAll(".m")
+        .data(mark)
+        .enter()
+        .append("image")
+        .attr("class", "marker")
+        .attr("width", 40)
+        .attr("height", 40)
+        .attr("xlink:href", "../static/images/wind.gif")
+        .style("cursor", "pointer")
+        .attr("transform", (d) => {
+            let p = projection([d.long, d.lat]);
+            return `translate(${p[0] - 22}, ${p[1] - 40})`;
+        });
+
+    svg.selectAll(".m")
+        .data(marks)
+        .enter()
+        .append("image")
+        .attr("class", "marker")
+        .attr("width", 30)
+        .attr("height", 30)
+        .attr("xlink:href", "../static/images/ar.svg")
+        .style("cursor", "pointer")
+        .attr("transform", (d) => {
+            let p = projection([d.long, d.lat]);
+            return `translate(${p[0] - 12}, ${p[1] - 30})`;
+        });
+
+    svg.selectAll("myPath")
+        .data(link)
+        .enter()
+        .append("path")
+        .attr("d", function (d) {
+            return path(d);
+        })
+        .style("fill", "none")
+        .style("stroke", "#69b3a2")
+        .style("stroke-width", 2);
+});
