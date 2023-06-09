aafffa7cf9f015c77590c268d7001ab4e88a274a
Narada Muni <naradamuni@Naradas-MacBook-Pro.local>
2023-06-09 11:04:33 +0800

    add detail info

--- script.js
+++ script.js
@@ -62,6 +62,17 @@ function animatePath(path) {
         });
 }
 
+const popover = d3
+    .select("svg")
+    .append("foreignObject")
+    .attr("class", "popover")
+    .attr("width", 210)
+    .attr("height", 100)
+    .style("opacity", 0)
+    .style("z-index", 9999);
+
+popover.append("xhtml:div").attr("class", "popover-content");
+
 d3.json("mn.geojson").then(function (data) {
     svg.append("g")
         .selectAll("path")
@@ -85,6 +96,29 @@ d3.json("mn.geojson").then(function (data) {
         .attr("transform", (d) => {
             let p = projection([d.long, d.lat]);
             return `translate(${p[0] - 22}, ${p[1] - 40})`;
+        })
+        .on("click", function (d) {
+            popover.transition().duration(200).style("opacity", 1);
+
+            const [markerX, markerY] = projection([d.srcElement.__data__.long, d.srcElement.__data__.lat]);
+            const [clickX, clickY] = d3.pointer(event);
+            const popoverX = markerX + clickX;
+            const popoverY = markerY + clickY;
+
+            popover.attr("x", popoverX - 150).attr("y", popoverY - 20);
+
+            popover.raise();
+
+            popover.select(".popover-content").html(`<div class='w-full h-full relative p-4 flex flex-row items-start gap-4'>
+                <div class='p-3 bg-white rounded-[10px]'>
+                    <image alt='' src='static/images/ar.svg' class='w-8' />
+                </div>
+                <div class='flex flex-col gap-2'>
+                    <p class='text-[14px] font-[600] leading-[16px]'>Mon naran</p>
+                    <p class='text-[12px] font-[500] leading-[16px]'>Capacity: 15 MWh</p>
+                    <p class='text-[12px] font-[500] leading-[16px]'>Daily yield: 5MWh</p>
+                </div>
+            </div>`);
         });
 
     svg.selectAll(".m")
@@ -99,6 +133,29 @@ d3.json("mn.geojson").then(function (data) {
         .attr("transform", (d) => {
             let p = projection([d.long, d.lat]);
             return `translate(${p[0] - 25}, ${p[1] - 40})`;
+        })
+        .on("click", function (d) {
+            popover.transition().duration(200).style("opacity", 1);
+
+            const [markerX, markerY] = projection([d.srcElement.__data__.long, d.srcElement.__data__.lat]);
+            const [clickX, clickY] = d3.pointer(event);
+            const popoverX = markerX + clickX;
+            const popoverY = markerY + clickY;
+
+            popover.attr("x", popoverX - 150).attr("y", popoverY - 20);
+
+            popover.raise();
+
+            popover.select(".popover-content").html(`<div class='w-full h-full relative p-4 flex flex-row items-start gap-4'>
+                <div class='p-3 bg-white rounded-[10px]'>
+                    <image alt='' src='static/images/ar.svg' class='w-8' />
+                </div>
+                <div class='flex flex-col gap-2'>
+                    <p class='text-[14px] font-[600] leading-[16px]'>Mon naran</p>
+                    <p class='text-[12px] font-[500] leading-[16px]'>Capacity: 15 MWh</p>
+                    <p class='text-[12px] font-[500] leading-[16px]'>Daily yield: 5MWh</p>
+                </div>
+            </div>`);
         });
 
     svg.selectAll("myPath")
